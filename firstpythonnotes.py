67187e7b2953fae5ff1357f77f06fc83a8d93a28
saikrishnalambo <73388700+saikrishnalambo@users.noreply.github.com>
2022-11-06 16:41:50 +0530

    Create firstpythonnotes.py

---
+++ firstpythonnotes.py
@@ -0,0 +1,27 @@
+# Declare & Assign Variables in python
+
+int_var = 10
+float_var = 18.25
+string_var = 'ineuron batch2' # or another way "ineuron batch2"
+bool_var = True # if we want to assign false then it should be like False
+
+
+# Function in python for Output
+# Function does what ? They might or minght not accept some parameter
+print("Hello World !!!!") 
+
+# Print variable values in python
+print("Value of int_var = ",int_var," - Result Done !!")
+print("Value of float_var = ",float_var," - Result Done !!")
+print("Value of string_var = ",string_var," - Result Done !!")
+print("Value of bool_var = ",bool_var," - Result Done !!")
+
+a = 3
+b = 10.5
+c = a+b
+print(c)
+
+#Type casting : Forcefully changing the data type of a variable is called type casting
+# int (), float(), str(), bool()
+d = int(c)
+print(type(d))
